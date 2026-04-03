a7d16219655032be2b46d3ad3284bc34f9c91c38
boutabacs <boutaba.cs@gmail.com>
2026-04-03 19:11:02 +0100

    update

--- hubrobe/src/requestMethods.js
+++ hubrobe/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://final-project-ang9.onrender.com/api/";
 
 // Pour les requêtes publiques (produits, etc.)
 export const publicRequest = axios.create({
